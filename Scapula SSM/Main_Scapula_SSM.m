6f15a80454573e0a30d0c6172659910b40567be2
Pendar Soltanmohammadi <63133937+pendarsm@users.noreply.github.com>
2021-05-24 23:48:13 +0430

    Add files via upload

--- Scapula SSM/Main_Scapula_SSM.m
+++ Scapula SSM/Main_Scapula_SSM.m
@@ -1,8 +1,8 @@
 % Developed by Pendar Soltanmohammadi, Western University, London, ON, Canada
 % This program takes modes of variation as input and returns the resulting geometry. 
-
-function Main_Scapula_SSM()
-
+clc;
+clear
+close all;
+global coeff Ave SD elements
+load 'Data_Shape_Scapula.mat'
 Scapula_SSM_App()
-
-end
