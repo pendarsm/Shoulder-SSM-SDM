f2b1a1e3a0f33164f31130e99ffc1490ad263aa5
Pendar Soltanmohammadi <63133937+pendarsm@users.noreply.github.com>
2021-05-24 23:47:11 +0430

    Add files via upload

--- Humerus SDM/Humerus_SDM.m
+++ Humerus SDM/Humerus_SDM.m
@@ -1,19 +1,9 @@
 % Developed by Pendar Soltanmohammadi, Western University, London, ON, Canada
 % This program takes modes of variation as input and returns the mean geometry mapped with the resulting density distribution. 
 function Humerus_SDM(input)
-
-load 'Data_Density_Humerus.mat'
-load 'PC_Sign_Humerus_Density.mat'
+global SD coeff Ave
 
 %input vector (modes of variation) should be described by perturbing the mean denisty distribution by +/- desired standard deviations (e.g. +1 standard deviation) for each of the first 10 (significant) principal components (PCs).
-
-[coeff,score,~,~,~,~] = pca(data_matrix);
-
-score_input = zeros(size(input));
-for i=1:length(input)
+score_input = input .* SD(1,1:length(input));  
     
- score_input(i) = input(i)*pc_sign_humerus_density(i)*std(score(:,i));   
-    
-end
-
-Write_VTK_Density('Average_Mesh_Humerus.vtk', 'Humerus_SDM_Output.vtk', (score_input*coeff(:,1:length(input))'+mean(data_matrix)),'Density');
+Write_VTK_Density('Average_Mesh_Humerus.vtk', 'Humerus_SDM_Output.vtk', (score_input * coeff(:,1:length(input))' + Ave),'Density');
--- Humerus SDM/Main_Humerus_SDM.m
+++ Humerus SDM/Main_Humerus_SDM.m
@@ -1,8 +1,8 @@
 % Developed by Pendar Soltanmohammadi, Western University, London, ON, Canada
 % This program takes modes of variation as input and returns the mean geometry mapped with the resulting density distribution. 
-
-function Main_Humerus_SDM()
-
+clc;
+clear
+close all;
+global coeff Ave SD
+load 'Data_Density_Humerus.mat'
 Humerus_SDM_App()
-
-end
