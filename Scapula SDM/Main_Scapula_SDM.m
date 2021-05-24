7ec69fdca0734c4e19d4a65accb2c7ecde7ec517
Pendar Soltanmohammadi <63133937+pendarsm@users.noreply.github.com>
2021-05-24 23:48:50 +0430

    Add files via upload

--- Scapula SDM/Main_Scapula_SDM.m
+++ Scapula SDM/Main_Scapula_SDM.m
@@ -1,8 +1,8 @@
 % Developed by Pendar Soltanmohammadi, Western University, London, ON, Canada
 % This program takes modes of variation as input and returns the mean geometry mapped with the resulting density distribution. 
-
-function Main_Scapula_SDM()
-
+clc;
+clear
+close all;
+global coeff Ave SD
+load 'Data_Density_Scapula.mat'
 Scapula_SDM_App()
-
-end
--- Scapula SDM/Scapula_SDM.m
+++ Scapula SDM/Scapula_SDM.m
@@ -1,19 +1,11 @@
 % Developed by Pendar Soltanmohammadi, Western University, London, ON, Canada
 % This program takes modes of variation as input and returns the mean geometry mapped with the resulting density distribution. 
 function Scapula_SDM(input)
+global SD coeff Ave
 
-load 'Data_Density_Scapula.mat'
-load 'PC_Sign_Scapula_Density.mat'
+%input vector (modes of variation) should be described by perturbing the mean denisty distribution by +/- desired standard deviations (e.g. +1 standard deviation) for each of the first 17 (significant) principal components (PCs).
+score_input = input .* SD(1,1:length(input));  
 
-[coeff,score,~,~,~,~] = pca(data_matrix);
-
-score_input = zeros(size(input));
-for i=1:length(input)
-    
- score_input(i) = input(i)*pc_sign_scapula_density(i)*std(score(:,i));   
-    
-end
-
-Write_VTK_Density('Average_Mesh_Scapula.vtk', 'Scapula_SDM_Output.vtk', (score_input*coeff(:,1:length(input))'+mean(data_matrix)),'Density');
+Write_VTK_Density('Average_Mesh_Scapula.vtk', 'Scapula_SDM_Output.vtk', (score_input * coeff(:,1:length(input))' + Ave),'Density');
 
 end
